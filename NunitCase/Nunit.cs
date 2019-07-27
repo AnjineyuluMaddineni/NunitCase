302ab5902f40cc21cbce2eb7abf4a00a9c5e8e7b
lenovo <lenovo@DESKTOP-UP83FQL>
2019-07-27 20:51:29 +0530

    01 commit Nunit Testing

---
+++ NunitCase/Nunit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NunitCase
+{
+    [TestFixture]
+    class Nunit
+    {
+        [TestCase]
+        public void Add()
+        {
+            TestCase test = new TestCase();
+            Assert.AreEqual(50, test.Add(25, 25));
+        }
+        [TestCase]
+        public void Sub()
+        {
+            TestCase test = new TestCase();
+            Assert.AreEqual(15, test.Sub(25, 10));
+        }
+        [TestCase]
+        public void Mul()
+        {
+            TestCase test = new TestCase();
+            Assert.AreEqual(100, test.Mul(25, 4));
+        }
+        [TestCase]
+        public void Div()
+        {
+            TestCase test = new TestCase();
+            Assert.AreEqual(50, test.Div(25, 25));
+        }
+
+    }
+}
--- NunitCase/Program.cs
+++ NunitCase/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,15 @@ using System.Threading.Tasks;
 
 namespace NunitCase
 {
+   
     class Program
     {
+     
         static void Main(string[] args)
-        {
+        {  
+
         }
+
+       
     }
 }
